c285760b3c5b0c30907d14943d8ed779bf99bf5f
ANASZAIDULLAH <bitm-f19-025@superior.edu.pk>
2022-03-30 18:00:53 +0500

    Sqlite

--- app/src/main/java/com/example/kiddy_kids/HomePage.java
+++ app/src/main/java/com/example/kiddy_kids/HomePage.java
@@ -1,14 +1,25 @@
 package com.example.kiddy_kids;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
+    CardView cate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        cate = (CardView) findViewById(R.id.cate);
+        cate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
 }
